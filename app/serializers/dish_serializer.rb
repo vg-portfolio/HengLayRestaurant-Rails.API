cd155765ca20432c3502d20af13ebf4f11725812
V4Vince <vince.guoo@gmail.com>
2017-08-29 13:58:09 -0400

    Adds createAt to dish serializer

--- app/serializers/dish_serializer.rb
+++ app/serializers/dish_serializer.rb
@@ -1,3 +1,3 @@
 class DishSerializer < ActiveModel::Serializer
-  attributes :id, :name, :khmer_name, :description, :price, :category_id
+  attributes :id, :name, :khmer_name, :description, :price, :category_id, :created_at
 end
