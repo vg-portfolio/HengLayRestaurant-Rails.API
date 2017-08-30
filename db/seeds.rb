0be0d96c458c54e4d5917c410b97b40dcec18f40
V4Vince <vince.guoo@gmail.com>
2017-08-29 20:47:20 -0400

    Adds seed file to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create({email:'henglayrestaurant@gmail.com', password:'henglayeee'})
