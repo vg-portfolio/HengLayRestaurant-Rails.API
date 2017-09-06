192fd3cc6e64c9a76323b41defaf00afef3b3806
V4Vince <vince.guoo@gmail.com>
2017-09-06 19:25:22 -0400

    Fixes conflicts b/w master and addsDishNum branch

--- app/serializers/dish_serializer.rb
+++ app/serializers/dish_serializer.rb
@@ -1,7 +1,3 @@
 class DishSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-  attributes :id, :name, :khmer_name, :description, :price, :category_id, :created_at
-=======
   attributes :id, :name, :khmer_name, :description, :price, :item_number, :category_id
->>>>>>> addsDishNum
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,31 +9,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-<<<<<<< HEAD
-
-# User.create({email:'henglayrestaurant@gmail.com', password:'henglayeee'})
-#
-# Category.create([
-#   { category_type: 'Appetizers'},
-#   { category_type: 'Salads'},
-#   { category_type: 'Noodle Soup'},
-#   { category_type: 'Fried Noodle'},
-#   { category_type: 'Rice Dish'},
-#   { category_type: 'Special Dinner Soup'},
-#   { category_type: 'Fried Fish'},
-#   { category_type: 'Pork, Chicken, Beef'},
-#   { category_type: 'Beverages'},
-#   { category_type: 'Traditional Khmer Food'}
-#   ])
-
-Dish.create([
-  { name: 'Spring Roll (3)',
-    description: 'Shredded lettuce, Cucumber, Mint combined with slice Pork and Shrimp. Then wrapped in soften rice paper.',
-    price: '6',
-    user_id: '1',
-    category_id: '1' },
-  ])
-=======
 users = User.create({
   email: 'henglayrestaurant@gmail.com',
   password: 'qwer'
