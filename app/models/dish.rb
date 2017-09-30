838fda6b09f1fe5ed716b4b5fd58bcb267a08ea2
V4Vince <vince.guoo@gmail.com>
2017-09-30 02:20:42 -0400

    Reformats money type

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,7 @@
 class Dish < ApplicationRecord
   attribute :price, :money # will behave like money type
+  attribute :price2, :money
+  attribute :price3, :money
   belongs_to :category
   belongs_to :user
 
